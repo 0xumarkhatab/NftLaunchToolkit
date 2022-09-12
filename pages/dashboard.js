57cf242c263a3350cfbc0c1045fb75df1b1755b3
umaresso <umarkhatab467@gmail.com>
2022-09-12 20:55:36 +0500

    Fetched and displayed BAYC

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,27 +7,55 @@ import {
   ButtonGroup,
   VStack,
   Text,
-
+  Grid,
+  GridItem,
+  Image,
 } from "@chakra-ui/react";
+
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
-import React, { useState ,useEffect} from "react";
-import { useAccount ,useConnect,useDisconnect} from "wagmi";
+import React, { useState, useEffect } from "react";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+
 function Dashboard() {
-  const { isConnected,isDisconnected } = useAccount();
+  const { isConnected, isDisconnected } = useAccount();
   const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const [collection, setCollection] = useState();
 
-  useEffect(()=>{
-    console.log("wallet connection ",isConnected);
-    if(isConnected)
-      setIsWalletConnected(true);
-      if(isDisconnected)
-      setIsWalletConnected(false);
+  async function fetchCollectionNFT(id) {
+    const options = { method: "GET" };
+    let nft = null;
+    return await fetch(
+      `https://api.opensea.io/api/v1/asset/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/${id}/?include_orders=false`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        nft = response;
+        return nft;
+      })
+      .catch((err) => console.error(err));
+  }
 
-  },[isConnected,isDisconnected])
+  async function fetchNFTs() {
+    let nfts = [];
+    for (let index = 0; index < 20; index++) {
+      const element = await fetchCollectionNFT(index);
+      nfts.push(element);
+    }
+    setCollection(nfts);
+  }
+
+  useEffect(() => {
+    fetchNFTs();
+    console.log("nfts are ", collection);
+    if (isConnected == true) setIsWalletConnected(true);
+
+    if (isDisconnected == true) setIsWalletConnected(false);
+  }, [isConnected, isDisconnected]);
 
   return (
-    <Box width="100%" height="100vh" bg="black">
+    <Box width="100%" height="fit-cpontent" bg="black">
       <HStack px={"2rem"} pt="20" width="95%" justify={"space-between"}>
         <Heading color="white" fontSize={["1rem", "2rem", "2rem"]}>
           Dashboard
@@ -45,7 +73,10 @@ function Dashboard() {
           </ButtonGroup>
         </Stack>
       </HStack>
-      <VStack justify="center" height={ isWalletConnected?"fit-content" : "90vh"}>
+      <VStack
+        justify="center"
+        height={collection?.length>0 ? "fit-content" : "90vh"}
+      >
         <Text
           bgGradient="linear(to-r,#84e1bc, #1652f0)"
           bgClip="text"
@@ -56,10 +87,40 @@ function Dashboard() {
         >
           {!isWalletConnected
             ? "Your Wallet is Not Connected ! Connect Wallet First"
-            : "My Minted NFTs"}
+            :  "My Minted NFTs"}
+            {
+              collection?.length<0 && " Loading.."
+            }
+
         </Text>
       </VStack>
-      
+      {isWalletConnected && (
+        <Grid p="10" templateColumns="repeat(3, 1fr)" gap={6}>
+          {collection?.map((item) => {
+            return (
+              <GridItem key={"item " + item.id}>
+                <Box p={"5"} border="1px solid gray" justifyContent={"center"} borderRadius={"10"}>
+                  <a
+                    target="_blank"
+                    href={`https://opensea.io/assets/ethereum/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/${item?.token_id}`}
+                  >
+                    <Image
+                      width="25vw"
+                      key={"img of " + item.id}
+                      src={item.image_url}
+                      alt={"item" + item.id}
+                    />
+                  </a>
+                  <Heading pt="4" fontSize={"16px"} color={"white"}>#{item.id}</Heading>
+
+
+                </Box>
+              </GridItem>
+            );
+            console.log("the image is ", item.image_url);
+          })}
+        </Grid>
+      )}
     </Box>
   );
 }
