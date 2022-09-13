635af5f272861550c95e1188714c6ae9054ab23c
umaresso <umarkhatab467@gmail.com>
2022-09-13 07:10:22 +0500

    Navbar Component Comments Added

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,66 +1,108 @@
 import { Box, HStack, Image } from "@chakra-ui/react";
 import React from "react";
+
 import Link from "next/link";
+
 import Head from "next/head";
+
 function Navbar() {
   return (
     <>
-    <Head>
-    <title>NFT Launch Kit</title>
+      {/*
+    Displaying Tab title of the page
+    */}
+      <Head>
+        <title>NFT Launch Kit</title>
+      </Head>
 
-    </Head>
-    <Box position={"fixed"} p={["0", "10px", "10px"]} bg={"white"}  boxShadow="1px 1px 1px 1px grey" w="100%">
-      <HStack  justify="space-between" w={["100%","98%","80vw"]}>
-        <a href="/">
-        <Image
-          pl={["10", "0", "20"]}
-          src="https://nftlaunchkit.xyz/logo.svg"
-          width={"25"}
-          height="8"
-          objectFit={"contain"}
-        />
+      {/* 
 
-        </a>
+        In Chakra UI , Alomost Everything is a Box
+        Analogeous to <div> in HTMl
 
-        <HStack
-          display={["none", "none", "flex"]}
-          spacing="10"
-          fontSize={"14px"}
-          fontWeight="620"
-          justify="center"
-        >
-          <NavLink title="Home" link={"/"} />
-          <NavLink title="Explore" link={"/"} />
-          <NavLink title="Pricing" link={"/"} />
-          <NavLink title="Membership" link={"/"} />
-          <NavLink title="StartBuilding" link={"/dashboard"} />
+        Chakra UI somehow is the combonation of HTMl and React Writing styles
+
+        Features :
+
+        - You can Create Custom Tags like Box ,Hstack etc.
+        - You can appply styles directly on the components
+        - Chakra UI also provides responsive tactics like
           
-        </HStack>
+          `p={["0", "10px", "10px"]}` 
+          
+          means the element ( on which it is applied ) will have the padding of 
+          
+          - 0px on Small sized screens 
+          - 10px on Medium sized screens 
+          - 10px on Large sized screens 
+          
 
-        <Box>
-          <Link href="/">
+*/}
+      <Box
+        position={"fixed"}
+        p={["0", "10px", "10px"]}
+        bg={"white"}
+        boxShadow="1px 1px 1px 1px grey"
+        w="100%"
+      >
+        {/* Cons */}
+
+        <HStack justify="space-between" w={["100%", "98%", "80vw"]}>
+          {/* Displaying the ICON of Website */}
+
+          <a href="/">
             <Image
-              display={["flex", "flex", "none"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/240px-Hamburger_icon.svg.png"
+              pl={["10", "0", "20"]}
+              src="https://nftlaunchkit.xyz/logo.svg"
               width={"25"}
               height="8"
+              objectFit={"contain"}
             />
-          </Link>
-        </Box>
-      </HStack>
-    </Box>
+          </a>
+
+          {/* Displaying Navifation Menu */}
+          <HStack
+            display={["none", "none", "flex"]}
+            spacing="10"
+            fontSize={"14px"}
+            fontWeight="620"
+            justify="center"
+          >
+            <NavLink title="Home" link={"/"} />
+            <NavLink title="Explore" link={"/"} />
+            <NavLink title="Pricing" link={"/"} />
+            <NavLink title="Membership" link={"/"} />
+            {/* All the Above links are Dummy redirecting to Home Page  */}
+
+            {/* This Page Will  show the Minted NFTs when the Wallet is Connected  */}
+            {/* Initially the Collection `Bored Ape` will be rendered */}
+            <NavLink title="StartBuilding" link={"/dashboard"} />
+          </HStack>
+
+{/* Dummy responsive Hamburger Menu */}
+          <Box>
+            <Link href="/">
+              <Image
+                display={["flex", "flex", "none"]}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/240px-Hamburger_icon.svg.png"
+                width={"25"}
+                height="8"
+              />
+            </Link>
+            
+          </Box>
+        </HStack>
+      </Box>
     </>
-    
   );
 }
 
-const NavLink = ({title,link})=>{
-    return (
-        <Box _hover={{textDecoration:"none"}}>
-        <Link href={link}>{title}</Link>
-        </Box>
-
-    )
-}
+const NavLink = ({ title, link }) => {
+  return (
+    <Box _hover={{ textDecoration: "none" }}>
+      <Link href={link}>{title}</Link>
+    </Box>
+  );
+};
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Box } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
-import Introduction from './components/Introduction';
-const Home = () => {
-  return (
-      <Introduction />
-  );
-};
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Box } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Introduction from "./components/Introduction";
 
+// The Root component from where the Entire Execution starts
+
+const Home = () => {
+  // initially we just have Introduction Component on Our Front Page
+  // Further Actions will be taken from Introduction Component or any other one e.g Navbar
+
+  return <Introduction />;
+};
 
 export default Home;
